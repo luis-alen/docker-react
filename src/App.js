688b0429820d9deab97509dd17af3438b557e3bd
Luis Alen <luisfernando.alen@gmail.com>
2020-03-13 17:27:32 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ronaldo brilha muito.
         </p>
         <a
           className="App-link"
