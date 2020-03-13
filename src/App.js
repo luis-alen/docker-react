beb1bf817020196c0fc13e1e35ccacea85715e3d
Luis Alen <luisfernando.alen@gmail.com>
2020-03-13 19:03:32 -0300

    changed App.jx text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ronaldo brilha muito.
+          Hey there.
         </p>
         <a
           className="App-link"
